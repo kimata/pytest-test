435c1efa03b9f4ed90a94a61695b27bd3e067977
KIMATA Tetsuya <kimata@green-rabbit.net>
2023-08-05 17:14:08 +0900

    fix

---
+++ README.md
@@ -0,0 +1,18 @@
+# Pytest のサンプル
+
+Python のプログラムのテストカバレッジをあげるのに使ったテクニックをまとめています．
+
+## 準備
+
+```bash
+poetry install
+```
+
+## 実行
+
+```bash
+poetry run pytest tests/test.py
+```
+
+
+
--- tests/test.py
+++ tests/test.py
@@ -134,6 +134,19 @@ def test_mock_return_value_5(mocker):
     assert b.b_func2() == "T2"
 
 
+# NOTE: 返値の置き換え (例外)
+def test_mock_return_value_6(mocker):
+    import a
+
+    # 置き換え前
+    assert a.a_func() == "called a_func"
+
+    mocker.patch("a.a_func", side_effect=RuntimeError)
+
+    with pytest.raises(RuntimeError):
+        a.a_func()
+
+
 # NOTE: オブジェクトの置き換え (メソッド及びプロパティを置き換え)
 def test_mock_object_1(mocker):
     import c
@@ -144,9 +157,7 @@ def test_mock_object_1(mocker):
     assert c_obj.prop == "prop C"
 
     mocker.patch.object(c.C, "c_func", return_value="T")
-    mocker.patch.object(
-        c.C, "prop", new_callable=mocker.PropertyMock, return_value="prop T"
-    )
+    mocker.patch.object(c.C, "prop", new_callable=mocker.PropertyMock, return_value="prop T")
 
     c_obj = c.C()
 
@@ -154,8 +165,50 @@ def test_mock_object_1(mocker):
     assert c_obj.prop == "prop T"
 
 
-# NOTE: オブジェクトの置き換え
-def test_mock_object_2(mocker):
+# NOTE: 応用編 (open の置き換え)
+def test_mock_open_1(mocker):
+    import builtins
+    import string
+    import random
+
+    # このファイル名が指定されたときに置き換えることにする
+    target_file = "".join(random.sample(string.ascii_lowercase, 10))
+
+    open_orig = builtins.open  # オリジナルの関数を保存する
+
+    file_mock = mocker.MagicMock()
+    file_mock.read.return_value = "T"
+
+    def open_mock(
+        file,
+        mode="r",
+        buffering=-1,
+        encoding=None,
+        errors=None,
+        newline=None,
+        closefd=True,
+        opener=None,
+    ):
+        # ファイル名が一致する場合のみ置換
+        if file == target_file:
+            return file_mock
+        else:
+            return open_orig(file, mode, buffering, encoding, errors, newline, closefd, opener)
+
+    mocker.patch("builtins.open", side_effect=open_mock)
+
+    # 存在しないファイルを読み書き
+    f = open(target_file)
+    f.write("T")
+    assert f.read() == "T"
+
+    # ファイル名がターゲットと異なる場合はエラーが出る
+    with pytest.raises(FileNotFoundError):
+        f = open(target_file + "_")
+
+
+# NOTE: 応用編 (open の置き換え)
+def test_mock_open_2(mocker):
     import builtins
     import string
     import random
@@ -164,9 +217,7 @@ def test_mock_object_2(mocker):
     target_file = "".join(random.sample(string.ascii_lowercase, 10))
 
     file_mock = mocker.MagicMock()
-    file_mock.read.return_value = "T"
-
-    # mocker.patch.object(open_mock, "write", return_value=None)
+    file_mock.__enter__.return_value.read.return_value = "T"
 
     open_orig = builtins.open
 
@@ -183,13 +234,51 @@ def test_mock_object_2(mocker):
         if file == target_file:
             return file_mock
         else:
-            return open_orig(
-                file, mode, buffering, encoding, errors, newline, closefd, opener
-            )
+            return open_orig(file, mode, buffering, encoding, errors, newline, closefd, opener)
 
     mocker.patch("builtins.open", side_effect=open_mock)
 
     # NOTE: 存在しないファイルを読み書き
-    f = open(target_file)
-    f.write("T")
-    assert f.read() == "T"
+
+    with open(target_file) as f:
+        f.write("T")
+        assert f.read() == "T"
+
+
+# NOTE: 応用編 (requests.get の置き換え)
+def test_mock_request(mocker):
+    import requests
+
+    res = requests.models.Response()
+    res._content_consumed = True
+    res._content = b'{"T": true}'
+    res.encoding = "utf-8"
+    res.status_code = 200
+
+    mocker.patch("requests.get", return_value=res)
+
+    assert requests.get("DUMMY").status_code == 200
+    assert requests.get("DUMMY").text == '{"T": true}'
+    assert requests.get("DUMMY").json() == {"T": True}
+
+
+# NOTE: 応用編 (Slack 通知の置き換え)
+@pytest.fixture(scope="session", autouse=True)
+def slack_mock():
+    with mock.patch(
+        "slack_sdk.web.client.WebClient.chat_postMessage",
+        retunr_value=True,
+    ) as fixture:
+        yield fixture
+
+
+def test_mock_slack(mocker):
+    import slack_sdk
+
+    client = slack_sdk.WebClient(token="DUMMY")
+    client.chat_postMessage(channel="DUMMY", text="DUMMY")
+
+
+# NOTE: 時間の操作
+def test_create_sensor_graph_1(freezer):
+    import schedule
